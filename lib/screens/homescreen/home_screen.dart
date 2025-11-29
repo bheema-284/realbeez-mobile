0d10302e3d426a92bb0938452221c9f2a4fe0a1b
bhargavirealbeez-01 <bhargavi.realbeez@gmail.com>
2025-11-29 12:22:15 +0530

    refactor: remove comment

--- lib/screens/homescreen/home_screen.dart
+++ lib/screens/homescreen/home_screen.dart
@@ -165,8 +165,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     }); 
     _startSpecialOffersAutoPlay(); 
     _startRecommendedSitesAutoPlay();
-    
-    // Simulate loading data
+
     _simulateDataLoading();
   } 
 
